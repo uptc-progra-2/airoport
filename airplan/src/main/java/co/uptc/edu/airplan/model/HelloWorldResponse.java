63bc8c19b2bb02ee71b889be836ffcdb08878b14
edgarmenesesc7@gmail.com <edgarmenesesc7@gmail.com>
2023-03-29 11:20:03 -0500

    initial commit

---
+++ airplan/src/main/java/co/uptc/edu/airplan/model/HelloWorldResponse.java
@@ -0,0 +1,24 @@
+package co.uptc.edu.airplan.model;
+
+public class HelloWorldResponse {
+	
+	String data;
+	
+	public HelloWorldResponse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public HelloWorldResponse(String data) {
+		super();
+		this.data = data;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+}
